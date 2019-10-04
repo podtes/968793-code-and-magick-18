796f04d968140ef5f5ab22eda888147f2aeba4b4
podtes <dpodtes@gmail.com>
2019-10-04 18:53:03 +0300

    исправил ошибки с наименованиями, выделил листенеры в отдельную функцию

--- js/setup.js
+++ js/setup.js
@@ -34,37 +34,34 @@ var FIREBALLS_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
  */
 
 /**
- * Функция возвращает случайный элемент из массива
  * @param {*[]} arrayName массив, из которого будем выбирать элемент
- * @return {object} randomValue случайный элемент массива
+ * @return {*} randomValue случайный элемент массива
  */
 var getRandomValue = function (arrayName) {
-  var randomValue = Math.floor(Math.random() * arrayName.length);
+  var randomValue = arrayName[Math.floor(Math.random() * arrayName.length)];
   return randomValue;
 };
 
 /**
- * Функция создает массив с объектами, описывающими параметры волшебников
- * @param {number} countWizards число необходимых волшебников
- * @return {[]} wizards массив c объектами, каждый из которых описывает одного волшебника
+ * @param {number} countWizards количество необходимых волшебников
+ * @return {Wizard[]} wizards массив c объектами, каждый из которых описывает одного волшебника
  */
 var createWizardsArray = function (countWizards) {
   for (var i = 0; i < countWizards; i++) {
     wizards[i] = {
-      name: WIZARD_NAMES[getRandomValue(WIZARD_NAMES)] + ' ' + WIZARD_LASTNAMES[getRandomValue(WIZARD_LASTNAMES)],
-      coatColor: COAT_COLORS[getRandomValue(COAT_COLORS)],
-      eyesColor: EYES_COLORS[getRandomValue(EYES_COLORS)]
+      name: getRandomValue(WIZARD_NAMES) + ' ' + getRandomValue(WIZARD_LASTNAMES),
+      coatColor: getRandomValue(COAT_COLORS),
+      eyesColor: getRandomValue(EYES_COLORS)
     };
   }
   return wizards;
 };
 
 /**
- * Функция генерирует DOM элемент, который описывает волшебника на основе шаблона similarWizardTemplate
  * @param {Wizard} wizard объект с исходными данными для генерации волшебника
  * @return {Node} wizardElement сгенерированный DOM элемент
  */
-var generateWizard = function (wizard) {
+var generateWizardHtmlElement = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -73,10 +70,14 @@ var generateWizard = function (wizard) {
   return wizardElement;
 };
 
-var putWizardsToPage = function () {
+/**
+ * @param {Wizard[]} wizardsArr
+ * return {void}
+ */
+var renderWizardHtmlElements = function (wizardsArr) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(generateWizard(wizards[i]));
+  for (var i = 0; i < wizardsArr.length; i++) {
+    fragment.appendChild(generateWizardHtmlElement(wizardsArr[i]));
   }
   similarListElement.appendChild(fragment);
 };
@@ -97,58 +98,61 @@ var popupClose = function () {
   document.removeEventListener('keydown', onOpenPopupEscPress);
 };
 
+var addListeners = function () {
+  setupOpenButton.addEventListener('click', function () {
+    popupOpen();
+  });
+
+  setupCloseButton.addEventListener('click', function () {
+    popupClose();
+  });
+
+  setupOpenButton.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      popupOpen();
+    }
+  });
+
+  setupCloseButton.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      popupClose();
+    }
+  });
+
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('У нас имя волшебника состоит как минимум из 2 букв!');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('В форме есть место всего под 25 символов, давай короче.');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Нужно дать волшебнику имя!');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  wizardCoat.addEventListener('click', function () {
+    wizardCoat.style.fill = getRandomValue(COAT_COLORS);
+    wizardCoatInput.value = wizardCoat.style.fill;
+  });
+
+  wizardEyes.addEventListener('click', function () {
+    wizardEyes.style.fill = getRandomValue(EYES_COLORS);
+    wizardEyesInput.value = wizardEyes.style.fill;
+  });
+
+  wizardFireball.addEventListener('click', function () {
+    var fireballRandomColor = getRandomValue(FIREBALLS_COLORS);
+    wizardFireball.style.background = fireballRandomColor;
+    wizardFireballInput.value = fireballRandomColor;
+  });
+};
+
 createWizardsArray(4);
 
-putWizardsToPage();
+renderWizardHtmlElements(wizards);
+
+addListeners();
 
 setupSimilar.classList.remove('hidden');
-
-setupOpenButton.addEventListener('click', function () {
-  popupOpen();
-});
-
-setupCloseButton.addEventListener('click', function () {
-  popupClose();
-});
-
-setupOpenButton.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    popupOpen();
-  }
-});
-
-setupCloseButton.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    popupClose();
-  }
-});
-
-userNameInput.addEventListener('invalid', function () {
-  if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('У нас имя волшебника состоит как минимум из 2 букв!');
-  } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('В форме есть место всего под 25 символов, давай короче.');
-  } else if (userNameInput.validity.valueMissing) {
-    userNameInput.setCustomValidity('Нужно дать волшебнику имя!');
-  } else {
-    userNameInput.setCustomValidity('');
-  }
-});
-
-wizardCoat.addEventListener('click', function () {
-  wizardCoat.style.fill = COAT_COLORS[getRandomValue(COAT_COLORS)];
-  wizardCoatInput.value = wizardCoat.style.fill;
-});
-
-wizardEyes.addEventListener('click', function () {
-  wizardEyes.style.fill = EYES_COLORS[getRandomValue(EYES_COLORS)];
-  wizardEyesInput.value = wizardEyes.style.fill;
-});
-
-
-wizardFireball.addEventListener('click', function () {
-  var fireballRandomColor = FIREBALLS_COLORS[getRandomValue(FIREBALLS_COLORS)];
-  wizardFireball.style.background = fireballRandomColor;
-  wizardFireballInput.value = fireballRandomColor;
-});
 
